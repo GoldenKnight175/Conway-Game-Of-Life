aa208cb38c8a37eed4f7ea3aab0a6fddd4cc4055
GoldenKnight175 <braveminer111@gmail.com>
2022-02-23 23:11:03 -0500

    I forgot to create the github before changing everything but I have created quite abit in this session.

--- Game of Life/UniverseSize.Designer.cs
+++ Game of Life/UniverseSize.Designer.cs
@@ -29,32 +29,46 @@
         private void InitializeComponent()
         {
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(135, 81);
+            this.numericUpDown1.Location = new System.Drawing.Point(80, 11);
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
             this.numericUpDown1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // UniverseSize
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(385, 233);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.numericUpDown1);
             this.Name = "UniverseSize";
             this.Text = "UniverseSize";
             this.Load += new System.EventHandler(this.UniverseSize_Load);
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Game of Life/UniverseSize.cs
+++ Game of Life/UniverseSize.cs
@@ -21,5 +21,10 @@ namespace Game_of_Life
         {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
